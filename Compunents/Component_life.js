72b0f7031141e6e505cba6d5e385e4b006ffc9a6
NguyenNgocLam <LamNN8@fpt.com.vn>
2019-05-10 22:37:27 +0700

    Update Component_life.js

--- Compunents/Component_life.js
+++ Compunents/Component_life.js
@@ -13,6 +13,7 @@ class App extends Component {
         console.log('jaiai')
         console.log('akakak')
         console.log('kdkdkdk')
+        console.log('lam')
         this.setState({data : this.state.data+1});
     }
     render(){
